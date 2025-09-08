651d437c7b87785c9a08494f24c7bae7d85581fe
kimHyeonJu <kimhyesom1202@gmail.com>
2025-09-08 15:06:27 +0900

    [Init] TailwindCSS 설치 및 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './common/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
@@ -15,13 +14,11 @@ const config: Config = {
       fontSize: {
       },
 
-      // 다크 모드용 색상 추가
       backgroundColor: {
       },
       textColor: {
       },
-
-      // 폰트 패밀리 추가
+      
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
       },
