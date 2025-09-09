64c178dffcb657331cd76408383759a14cabf8b2
kimHyeonJu <kimhyesom1202@gmail.com>
2025-09-09 16:44:12 +0900

    [Cleanup] 필요 없는 div태그 삭제

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -5,12 +5,10 @@ interface BadgeProps {
 
 export default function Badge({ children, active = false }: BadgeProps) {
   return (
-    <div>
-      <span
-        className={`text-16 px-1.5 py-1 rounded-sm ${active ? 'bg-gray-200 text-gray-700 font-bold' : 'bg-white text-gray-600 font-normal'}`}
-      >
-        {children}
-      </span>
-    </div>
+    <span
+      className={`text-16 px-1.5 py-1 rounded-sm ${active ? 'bg-gray-200 text-gray-700 font-bold' : 'bg-white text-gray-600 font-normal'}`}
+    >
+      {children}
+    </span>
   );
 }
