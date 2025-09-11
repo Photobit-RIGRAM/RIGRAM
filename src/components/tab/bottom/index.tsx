fb2b9e92b85e0f74da167b81876473579bc1b081
kimHyeonJu <kimhyesom1202@gmail.com>
2025-09-11 16:26:07 +0900

    [Feature] Tab 컴포넌트
    1. Top Tab
    2. Bottom Tab
    2-1) 학과페이지에서 쓰이는 탭
    2-2) 조직페이지에서 쓰이는 탭
    2-3) 학교 소개 관리에서 쓰이는 탭
    3. 모바일/태블릿 이후 반응형

---
+++ src/components/tab/bottom/index.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+
+const BOTTOM_TAB = {
+  department: [
+    { id: '0', title: '기본 정보', url: '#' },
+    { id: '1', title: '소개', url: '#' },
+    { id: '2', title: '교직원', url: '#' },
+    { id: '3', title: '학생회', url: '#' },
+  ],
+  organization: [
+    { id: '0', title: '전체', url: '#' },
+    { id: '1', title: '팀', url: '#' },
+    { id: '2', title: '단체', url: '#' },
+    { id: '3', title: '동아리', url: '#' },
+    { id: '4', title: '행사', url: '#' },
+  ],
+  school_intro: [
+    { id: '0', title: '전경', url: '#' },
+    { id: '1', title: '연혁', url: '#' },
+    { id: '2', title: '상징', url: '#' },
+    { id: '3', title: '임원진', url: '#' },
+  ],
+};
+
+export default function BottomTab({ purpose }: { purpose: keyof typeof BOTTOM_TAB }) {
+  const [activeTab, setActiveTab] = useState('0');
+
+  const handleTab = (id: string) => {
+    setActiveTab(id);
+  };
+
+  const getTabData = () => {
+    return BOTTOM_TAB[purpose] || [];
+  };
+
+  const tabData = getTabData();
+
+  return (
+    <ul className="inline-flex items-center gap-1 w-fit p-1 bg-tab-bg-bottom rounded-4xl">
+      {tabData.map((tab) => (
+        <li key={tab.id} className="inline-flex">
+          <Link
+            href={tab.url}
+            className={`text-14 md:text-16 text-white rounded-4xl px-4 py-2 md:px-6 md:py-2 ${
+              activeTab === tab.id ? 'bg-tab-bg-bottom-active font-bold' : 'font-medium'
+            } hover:bg-tab-bg-bottom-active hover:font-bold focus:bg-tab-bg-bottom-active focus:font-bold active:bg-tab-bg-bottom-active active:font-bold`}
+            onClick={() => handleTab(tab.id)}
+          >
+            {tab.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/tab/top/index.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+
+const TOP_TAB = [
+  { id: '0', title: '학과 관리', url: '#' },
+  { id: '1', title: '졸업생 관리', url: '#' },
+  { id: '2', title: '단체 사진', url: '#' },
+  { id: '3', title: '학교 소개 관리', url: '#' },
+];
+
+export default function TopTab() {
+  const [isActive, setIsActive] = useState('0');
+  const handleTab = (id: string) => {
+    setIsActive(id);
+  };
+
+  return (
+    <ul className="flex items-center gap-2 md:gap-4">
+      {TOP_TAB.map((tab) => (
+        <li key={tab.id}>
+          <Link
+            href={tab.url}
+            className={`text-14 md:text-16 rounded-md px-2 py-2 md:px-4 md:py-2.5 ${isActive === tab.id ? 'bg-gray-200 text-gray-800 font-bold' : 'text-gray-600 font-medium'} hover:bg-gray-200 hover:text-gray-800 hover:font-bold focus:bg-gray-200 focus:text-gray-800 focus:font-bold active:bg-gray-200 active:text-gray-800 active:font-bold`}
+            onClick={() => handleTab(tab.id)}
+          >
+            {tab.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
