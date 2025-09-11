c4123b82f988b897a154d7e8fd38e9b1c5a0c758
kimHyeonJu <kimhyesom1202@gmail.com>
2025-09-11 13:46:03 +0900

    [Feature] Header 컴포넌트
    1. 학교 선택이 되어있을 경우
    2. 학교 선택이 되어있지 않을 경우
    3. 로고 선택 시 로그아웃/main 페이지 이동 dropdown 생성
    4. 레이아웃 적용
    5. 모바일/태블릿 이후 반응형

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/header';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-screen">
-      <Header />
+      <Header hasSchool />
       <main className="px-5 py-5 md:px-0 md:py-0 flex justify-center items-center flex-1">
         {children}
       </main>
