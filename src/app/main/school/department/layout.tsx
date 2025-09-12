4aca409c53f78d2783169a5c993faa7d32990e41
kimHyeonJu <kimhyesom1202@gmail.com>
2025-09-12 14:40:29 +0900

    [Feature] 학과 교직원 페이지 레이아웃

--- src/app/main/school/department/layout.tsx
+++ src/app/main/school/department/layout.tsx
@@ -6,14 +6,14 @@ import { usePathname } from 'next/navigation';
 
 export default function DepartmentLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const hasPathname = pathname.includes('/department/detail');
+  const hasPathname = pathname.includes('/department');
 
   return (
-    <div className="relative flex flex-col gap-8 w-full h-full md:px-10">
+    <div className="relative flex flex-col gap-4 w-full h-full md:px-10 md:gap-8">
       <header className="flex justify-center md:justify-end items-center mt-4">
         <TopTab />
       </header>
-      <main className="flex flex-col items-center gap-6 w-full h-full m-auto md:max-w-[1080px]">
+      <main className="flex flex-col items-center gap-4 w-full h-full m-auto md:max-w-[1080px] md:gap-6">
         {children}
         {hasPathname && <BottomTab purpose="department" />}
       </main>
