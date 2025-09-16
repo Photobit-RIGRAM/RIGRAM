b508ebcd19248a2d7a0459e6fab5ea882486fd67
kimHyeonJu <kimhyesom1202@gmail.com>
2025-09-16 18:35:24 +0900

    [Style] button 컴포넌트 수정
    - hover, focus, focus-visible, active 시 스타일 추가

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -8,6 +8,9 @@ interface ButtonProps {
 }
 
 export default function Button({ children, onClick, href, className }: ButtonProps) {
+  const PseudoSelectors =
+    'hover:font-bold hover:outline hover:outline-primary-700 focus:font-bold focus:outline focus:outline-primary-700 focus-visible:font-bold focus-visible:outline focus-visible:outline-primary-700 active:font-bold active:outline active:outline-primary-700';
+
   if (href) {
     return (
       <Link href={href} className={`flex justify-center items-center ${className ?? ''}`}>
@@ -17,7 +20,10 @@ export default function Button({ children, onClick, href, className }: ButtonPro
   }
 
   return (
-    <button className={`flex justify-center items-center ${className ?? ''}`} onClick={onClick}>
+    <button
+      className={`flex justify-center items-center ${className ?? ''} ${PseudoSelectors}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
