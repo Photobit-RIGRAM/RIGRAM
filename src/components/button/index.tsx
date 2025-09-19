25bb7b578d3662c0b1e96e74467100fb5f6df712
kimHyeonJu <kimhyesom1202@gmail.com>
2025-09-19 10:16:01 +0900

    [Refactor] Button 컴포넌트 리팩토링

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,12 +2,21 @@ import Link from 'next/link';
 
 interface ButtonProps {
   children: React.ReactNode;
+  type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
   href?: string;
   className?: string;
+  disabled?: boolean;
 }
 
-export default function Button({ children, onClick, href, className }: ButtonProps) {
+export default function Button({
+  children,
+  type,
+  onClick,
+  href,
+  className,
+  disabled,
+}: ButtonProps) {
   const PseudoSelectors =
     'hover:font-bold hover:outline hover:outline-primary-700 focus:font-bold focus:outline focus:outline-primary-700 focus-visible:font-bold focus-visible:outline focus-visible:outline-primary-700 active:font-bold active:outline active:outline-primary-700';
 
@@ -21,8 +30,10 @@ export default function Button({ children, onClick, href, className }: ButtonPro
 
   return (
     <button
+      type={type}
       className={`flex justify-center items-center ${className ?? ''} ${PseudoSelectors}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
