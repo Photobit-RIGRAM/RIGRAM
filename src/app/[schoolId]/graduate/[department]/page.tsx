8bd8ef1ce119861c8106cc081e1eaf82ec19df91
kimHyeonJu <kimhyesom1202@gmail.com>
2025-09-26 18:24:37 +0900

    [Feature] 졸업생 리스트 페이지, Profile 컴포넌트
    - 해당 학과의 졸업생 리스트 출력
    - 클릭 시, 졸업생 상세 정보 페이지로 이동
    - 해당 학과의 학생들 데이터를 Profile 컴포넌트에서 데이터 출력

--- src/app/[schoolId]/graduate/[department]/page.tsx
+++ src/app/[schoolId]/graduate/[department]/page.tsx
@@ -1,15 +1,54 @@
+'use client';
+
 import Button from '@/components/button';
 import Profile from '@/components/profile';
+import { useCollegeStore } from '@/store/useCollegeStore';
+import { useDepartmentStore } from '@/store/useDepartmentStore';
+import { useStudentStore } from '@/store/useStudentStore';
 import { ArrowLeft, ListFilter, Search } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function GraduateDepartmentStudentList() {
+  const router = useRouter();
+  const [deptName, setDeptName] = useState<string | null>(null);
+  const pathname = usePathname();
+  const segments = pathname.split('/').filter(Boolean);
+  // const schoolId = segments[0];
+  const departmentId = segments[2];
+  const fetchCollegeById = useCollegeStore((state) => state.fetchCollegeById);
+  const fetchDepartmentById = useDepartmentStore((state) => state.fetchDepartmentById);
+  const { students, fetchStudents, isLoading } = useStudentStore();
+
+  useEffect(() => {
+    if (departmentId) {
+      fetchDepartmentById(departmentId).then((dept) => {
+        setDeptName(dept?.name || null);
+      });
+    }
+  }, [departmentId, fetchDepartmentById]);
+
+  useEffect(() => {
+    if (departmentId) {
+      fetchStudents(departmentId);
+    }
+  }, [departmentId, fetchStudents]);
+
+  const handleGoBack = () => {
+    router.back();
+  };
+  const handleGoProfile = (id: string) => {
+    router.push(`${pathname}/${id}`);
+  };
+
   return (
     <section className="relative w-full h-full bg-white border border-border p-5 rounded-xl shadow-dropdown md:p-10">
       <div className="z-10 relative mb-4 md:absolute md:left-0 md:top-0 md:transform md:-translate-y-12 flex justify-start items-center gap-1 text-gray-900 md:px-0 md:pt-0">
-        <Button href={'/main/school/department'}>
+        <Button onClick={handleGoBack}>
           <ArrowLeft className="w-8 h-8" />
         </Button>
-        <h3 className="text-20 font-semibold">학과이름들어가는곳</h3>
+        <h2 className="text-20 font-semibold">{deptName}</h2>
       </div>
       <div className="flex flex-col gap-5">
         <div className="flex justify-between items-center">
@@ -25,10 +64,9 @@ export default function GraduateDepartmentStudentList() {
           </div>
         </div>
         <div className="grid grid-cols-1 gap-2 md:grid-cols-4 md:gap-4">
-          <Profile />
-          <Profile />
-          <Profile />
-          <Profile />
+          {students.map((student) => (
+            <Profile key={student.id} {...student} onClick={() => handleGoProfile(student.id)} />
+          ))}
         </div>
       </div>
     </section>
--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,21 +1,40 @@
-import { STUDENT_DATA } from '@/mock/mockData';
+import type { Student } from '@/types/student';
 
-export default function Profile() {
+interface ProfileProps extends Student {
+  onClick: () => void;
+}
+
+export default function Profile({
+  id,
+  school_id,
+  dept_id,
+  name,
+  name_en,
+  graduation_year,
+  profile_default,
+  profile_graduate,
+  created_at,
+  updated_at,
+  onClick,
+}: ProfileProps) {
   return (
-    <div className="flex flex-col gap-4 p-4 bg-white border border-border rounded-xl">
+    <div
+      className="flex flex-col gap-4 p-4 bg-white border border-border rounded-xl"
+      onClick={onClick}
+    >
       <div className="flex gap-3 justify-center items-center w-full overflow-hidden">
         <img
-          src={STUDENT_DATA.photo_id}
-          alt={`${STUDENT_DATA.name}의 증명사진`}
+          src={profile_default}
+          alt={`${name}의 증명사진`}
           className="w-1/2 h-35 rounded-sm object-cover"
         />
         <img
-          src={STUDENT_DATA.photo_graduation}
-          alt={`${STUDENT_DATA.name}의 졸업사진`}
+          src={profile_graduate}
+          alt={`${name}의 졸업사진`}
           className="w-1/2 h-35 rounded-sm object-cover"
         />
       </div>
-      <span className="text-18 text-gray-900 font-semibold">{STUDENT_DATA.name}</span>
+      <span className="text-18 text-gray-900 font-semibold">{name}</span>
     </div>
   );
 }
