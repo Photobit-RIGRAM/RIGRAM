a80cb6f01bf9030b669292519994a39d458d1cb1
kimHyeonJu <kimhyesom1202@gmail.com>
2025-10-05 21:38:24 +0900

    [Refactor] RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,46 @@
+'use client';
+
 import './globals.css';
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { useAuthStore } from '@/store/useAuthStore';
+import { supabase } from '@/utils/supabase/client';
+// import type { Metadata } from 'next';
+// import localFont from 'next/font/local';
+import { useEffect } from 'react';
 
-const pretendard = localFont({
-  src: './fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '100 900',
-  variable: '--font-pretendard',
-});
-
-export const metadata: Metadata = {
-  title: 'RIGRAM',
-  description: 'Photobit에서 제작한 학교별 졸업 앨범',
-  icons: {
-    icon: '/favicon.ico',
-  },
-};
+// export const metadata: Metadata = {
+//   title: 'RIGRAM',
+//   description: 'Photobit에서 제작한 학교별 졸업 앨범',
+//   icons: {
+//     // icon: '/favicon.ico',
+//   },
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isLogin = useAuthStore((state) => state.isLogin);
+  const user = useAuthStore((state) => state.user);
+  const setUser = useAuthStore((state) => state.setUser);
+  const fetchUser = useAuthStore((state) => state.fetchUser);
+
+  useEffect(() => {
+    // 앱 첫 로드 시 로그인 상태 확인
+    fetchUser();
+
+    // 로그인/로그아웃 이벤트 구독
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, [fetchUser, setUser]);
+
   return (
-    <html lang="ko" className={`${pretendard.variable} antialiased`}>
+    <html lang="ko">
       <body>{children}</body>
     </html>
   );
