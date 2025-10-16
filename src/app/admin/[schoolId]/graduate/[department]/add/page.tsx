db28c05e78f494131da2b2144b2ef341adade23f
kimHyeonJu <kimhyesom1202@gmail.com>
2025-10-16 15:37:59 +0900

    [Feature] 졸업생 추가 페이지
    - 핸드폰, 이메일, 졸업연도 추가
    - 추가 후 이동 url 변경

--- src/app/admin/[schoolId]/graduate/[department]/add/page.tsx
+++ src/app/admin/[schoolId]/graduate/[department]/add/page.tsx
@@ -2,61 +2,76 @@
 
 import Badge from '@/components/badge';
 import Button from '@/components/button';
+import Divider from '@/components/divider';
 import FileInput from '@/components/fileInput';
 import Input from '@/components/input';
+import PageHeader from '@/components/pageHeader';
 import { useDepartmentStore } from '@/store/useDepartmentStore';
 import { useSchoolStore } from '@/store/useSchoolStore';
 import { useStudentStore } from '@/store/useStudentStore';
 import { supabase } from '@/utils/supabase/client';
-import { ArrowLeft, Asterisk } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Asterisk } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 export default function GraduateAddPage() {
   const router = useRouter();
   const pathname = usePathname();
-  const segments = pathname.split('/').filter(Boolean);
+  const segments = useMemo(() => pathname.split('/').filter(Boolean), [pathname]);
   const currentSchoolId = segments[1];
   const currentDeptId = segments[3];
+
   const [studentName, setStudentName] = useState('');
   const [studentNameEn, setStudentNameEn] = useState('');
   const [deptName, setDeptName] = useState('');
+  const [deptNameEn, setDeptNameEn] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [graduationYear, setGraduationYear] = useState('');
   const [profileImg, setProfileImg] = useState<File | null>(null);
   const [graduationImg, setGraduationImg] = useState<File | null>(null);
+
   const fetchDepartmentById = useDepartmentStore((state) => state.fetchDepartmentById);
   const addStudentProfile = useStudentStore((state) => state.addStudentProfile);
   const school = useSchoolStore((state) => state.school);
   const schoolNameEn = school?.school_name_en || '';
-  const [deptNameEn, setDeptNameEn] = useState('');
 
-  useEffect(() => {
-    if (currentDeptId) {
-      fetchDepartmentById(currentDeptId).then((dept) => {
-        if (dept?.name) {
-          setDeptName(dept.name);
-          setDeptNameEn(dept.name_en);
-        }
-      });
-    }
-  }, [currentDeptId, fetchDepartmentById]);
+  const slugify = (text: string) =>
+    text
+      .toLowerCase()
+      .trim()
+      .replace(/[\s\W-]+/g, '-');
 
-  const handleGoBack = () => {
-    router.back();
-  };
-
-  const handleFileSelect = (file: File | null, type: 'profile' | 'graduation') => {
+  // 파일 선택 handler
+  const handleFileSelect = useCallback((file: File | null, type: 'profile' | 'graduation') => {
     if (!file) return;
     if (type === 'profile') {
       setProfileImg(file);
     } else {
       setGraduationImg(file);
     }
-  };
+  }, []);
+
+  // 학과명 가져오기
+  useEffect(() => {
+    if (currentDeptId) {
+      fetchDepartmentById(currentDeptId).then((dept) => {
+        if (dept?.name) {
+          setDeptName(dept.name);
+          setDeptNameEn(dept.name_en);
+          setGraduationYear(dept.graduation_year);
+        }
+      });
+    }
+  }, [currentDeptId, fetchDepartmentById]);
 
   /* 파일 업로드 (Supabase storage) */
   const uploadImage = async (file: File, folder: string) => {
-    const filePath = `${schoolNameEn}/${deptNameEn}/${studentNameEn}/${folder}/${file.name}`;
+    const schoolName = slugify(schoolNameEn);
+    const deptName = slugify(deptNameEn);
+    const studentName = slugify(studentNameEn);
+
+    const filePath = `${schoolName}/${deptName}/${studentName}/${folder}/${file.name}`;
 
     const { error } = await supabase.storage
       .from('student-profiles')
@@ -92,11 +107,14 @@ export default function GraduateAddPage() {
         studentName,
         studentNameEn,
         profileUrl,
-        graduationUrl
+        graduationUrl,
+        phone,
+        email,
+        graduationYear
       );
 
       alert('학생 프로필이 추가되었습니다.');
-      router.back();
+      router.replace(`/admin/${school?.id}/graduate/${currentDeptId}`);
     } catch (error) {
       console.error('Unexpected error : ', error);
       alert('학생 프로필 등록 중 오류가 발생했습니다.');
@@ -104,129 +122,183 @@ export default function GraduateAddPage() {
   };
 
   return (
-    <section className="relative bg-white w-full p-4 md:p-10 border border-gray-200 rounded-xl shadow-dropdown md:w-[1080px] md:min-h-[753px]">
-      <div className="z-10 absolute left-0 top-0 transform -translate-y-12 flex justify-start items-center gap-1 text-gray-900">
-        <Button onClick={handleGoBack}>
-          <ArrowLeft className="w-8 h-8" />
-        </Button>
-        <h3 className="text-20 font-semibold">졸업생 추가하기</h3>
-      </div>
-      <header className="relative flex flex-col justify-start gap-1">
-        <ol className="flex justify-start items-center">
-          <li>
-            <Badge active>졸업생 정보</Badge>
-          </li>
-        </ol>
-        <h3 className="text-24 text-gray-900 font-semibold">졸업생 정보</h3>
-        <div className="absolute right-0 bottom-0 flex items-center gap-2">
-          <Button
-            className="text-white bg-primary-700 rounded-lg px-3 py-1.5"
-            onClick={handleProfileAdd}
-          >
-            추가하기
-          </Button>
-        </div>
-      </header>
-      <span className="inline-block w-full h-px bg-gray-200 my-8" aria-hidden="true"></span>
-      <div className="flex flex-col gap-6">
-        <div className="flex justify-start items-center w-full">
-          <label
-            htmlFor="graduate-name"
-            className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
-          >
-            이름
-            <Asterisk className="text-red w-4 h-4" />
-          </label>
-          <div className="flex-1 min-w-0">
-            <Input
-              purpose="text"
-              id="graduate-name"
-              placeholder="졸업생의 이름을 입력해 주세요.(80자 제한)"
-              className="w-full"
-              value={studentName}
-              onChange={(e) => setStudentName(e.target.value)}
-            />
+    <>
+      <PageHeader title="졸업생 추가하기" />
+      <section className="relative bg-white w-full p-4 md:p-10 border border-gray-200 rounded-xl shadow-dropdown md:w-[1080px] md:min-h-[753px]">
+        <header className="relative flex flex-col justify-start gap-1">
+          <ol className="flex justify-start items-center">
+            <li>
+              <Badge active>졸업생 정보</Badge>
+            </li>
+          </ol>
+          <h3 className="text-24 text-gray-900 font-semibold">졸업생 정보</h3>
+          <div className="absolute right-0 bottom-0 flex items-center gap-2">
+            <Button
+              className="text-white bg-primary-700 rounded-lg px-3 py-1.5"
+              onClick={handleProfileAdd}
+              aria-label="졸업생 추가하기"
+            >
+              추가하기
+            </Button>
+          </div>
+        </header>
+        <Divider gap={6} mdGap={8} />
+        <div className="flex flex-col gap-6">
+          <div className="flex justify-start items-center w-full">
+            <label
+              htmlFor="graduate-name"
+              className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
+            >
+              이름
+              <Asterisk className="text-red w-4 h-4" />
+            </label>
+            <div className="flex-1 min-w-0">
+              <Input
+                purpose="text"
+                id="graduate-name"
+                placeholder="졸업생의 이름을 입력해 주세요.(80자 제한)"
+                className="w-full"
+                value={studentName}
+                onChange={(e) => setStudentName(e.target.value)}
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="flex justify-start items-center w-full">
+            <label
+              htmlFor="graduate-en-name"
+              className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
+            >
+              영어 이름
+              <Asterisk className="text-red w-4 h-4" />
+            </label>
+            <div className="flex-1 min-w-0">
+              <Input
+                purpose="text"
+                id="graduate-en-name"
+                placeholder="졸업생의 영어 이름을 입력해 주세요.(80자 제한)"
+                className="w-full"
+                value={studentNameEn}
+                onChange={(e) => setStudentNameEn(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex justify-start items-center w-full">
+            <label
+              htmlFor="department-name"
+              className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
+            >
+              학과
+            </label>
+            <div className="flex-1 min-w-0">
+              <Input
+                purpose="text"
+                id="department-name"
+                placeholder="학과 이름을 입력해 주세요.(80자 제한)"
+                className="w-full"
+                value={deptName}
+                readOnly={true}
+              />
+            </div>
+          </div>
+          <div className="flex justify-start items-center w-full">
+            <label
+              htmlFor="graduation-year"
+              className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
+            >
+              졸업 연도
+            </label>
+            <div className="flex-1 min-w-0">
+              <Input
+                purpose="text"
+                id="graduation-year"
+                placeholder=""
+                className="w-full"
+                value={graduationYear}
+                readOnly={true}
+              />
+            </div>
+          </div>
+          <div className="flex justify-start items-center w-full">
+            <label
+              htmlFor="contact"
+              className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
+            >
+              연락처
+            </label>
+            <div className="flex-1 min-w-0">
+              <Input
+                purpose="text"
+                id="contact"
+                placeholder="01012345678"
+                className="w-full"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex justify-start items-center w-full">
+            <label
+              htmlFor="email"
+              className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
+            >
+              이메일
+            </label>
+            <div className="flex-1 min-w-0">
+              <Input
+                purpose="text"
+                id="email"
+                placeholder="example@email.com"
+                className="w-full"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex justify-start items-start">
+            <label
+              htmlFor="profile-img"
+              className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
+            >
+              프로필 이미지
+              <Asterisk className="text-red w-4 h-4" />
+            </label>
+            <div className="flex-1 min-w-0">
+              <FileInput
+                id="profile-img"
+                className="w-full h-full"
+                size="lg"
+                onChange={(files) => {
+                  if (!files) return;
+                  const file = files instanceof FileList ? files[0] : files;
+                  handleFileSelect(file, 'profile');
+                }}
+              />
+            </div>
+          </div>
+          <div className="flex justify-start items-start">
+            <label
+              htmlFor="graduation-img"
+              className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
+            >
+              학사모 이미지
+              <Asterisk className="text-red w-4 h-4" />
+            </label>
+            <div className="flex-1 min-w-0">
+              <FileInput
+                id="graduation-img"
+                className="w-full h-full"
+                size="lg"
+                onChange={(files) => {
+                  if (!files) return;
+                  const file = files instanceof FileList ? files[0] : files;
+                  handleFileSelect(file, 'graduation');
+                }}
+              />
+            </div>
           </div>
         </div>
-        <div className="flex justify-start items-center w-full">
-          <label
-            htmlFor="graduate-en-name"
-            className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
-          >
-            영어 이름
-            <Asterisk className="text-red w-4 h-4" />
-          </label>
-          <div className="flex-1 min-w-0">
-            <Input
-              purpose="text"
-              id="graduate-en-name"
-              placeholder="졸업생의 영어 이름을 입력해 주세요.(80자 제한)"
-              className="w-full"
-              value={studentNameEn}
-              onChange={(e) => setStudentNameEn(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="flex justify-start items-center w-full">
-          <label
-            htmlFor="department-name"
-            className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
-          >
-            학과
-          </label>
-          <div className="flex-1 min-w-0">
-            <Input
-              purpose="text"
-              id="department-name"
-              placeholder="학과 이름을 입력해 주세요.(80자 제한)"
-              className="w-full"
-              value={deptName}
-            />
-          </div>
-        </div>
-        <div className="flex justify-start items-start">
-          <label
-            htmlFor="profile-img"
-            className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
-          >
-            프로필 이미지
-            <Asterisk className="text-red w-4 h-4" />
-          </label>
-          <div className="flex-1 min-w-0">
-            <FileInput
-              id="profile-img"
-              className="w-full h-full"
-              size="lg"
-              onChange={(files) => {
-                if (!files) return;
-                const file = files instanceof FileList ? files[0] : files;
-                handleFileSelect(file, 'profile');
-              }}
-            />
-          </div>
-        </div>
-        <div className="flex justify-start items-start">
-          <label
-            htmlFor="graduation-img"
-            className="shrink-0 flex justify-start items-center gap-0.5 text-16 text-gray-800 w-[100px] md:text-18 md:w-[200px]"
-          >
-            학사모 이미지
-            <Asterisk className="text-red w-4 h-4" />
-          </label>
-          <div className="flex-1 min-w-0">
-            <FileInput
-              id="graduation-img"
-              className="w-full h-full"
-              size="lg"
-              onChange={(files) => {
-                if (!files) return;
-                const file = files instanceof FileList ? files[0] : files;
-                handleFileSelect(file, 'graduation');
-              }}
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
