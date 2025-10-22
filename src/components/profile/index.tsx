b04ea3547bf3ad339de6b597e4e5a5c045f1f0f6
kimHyeonJu <kimhyesom1202@gmail.com>
2025-10-22 11:08:35 +0900

    [Fix] Image 태그
    - fill과 함꼐 사용하는 sizes 속성 추가

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -31,10 +31,22 @@ export default function Profile({
     >
       <div className="flex gap-3 justify-center items-center w-full overflow-hidden">
         <div className="relative w-1/2 h-35 rounded-sm ">
-          <Image src={profile_default} alt={`${name}의 증명사진`} fill className="object-cover" />
+          <Image
+            src={profile_default}
+            alt={`${name}의 증명사진`}
+            fill
+            sizes="(max-width: 768px) w-1/2 h-35"
+            className="object-cover"
+          />
         </div>
         <div className="relative w-1/2 h-35 rounded-sm ">
-          <Image src={profile_graduate} alt={`${name}의 졸업사진`} fill className="object-cover" />
+          <Image
+            src={profile_graduate}
+            alt={`${name}의 졸업사진`}
+            fill
+            sizes="(max-width: 768px) w-1/2 h-35"
+            className="object-cover"
+          />
         </div>
       </div>
       <span className="text-18 text-gray-900 font-semibold">{name}</span>
