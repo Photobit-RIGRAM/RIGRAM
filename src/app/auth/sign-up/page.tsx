6278dd93fc2891a1026c601f97798b06ea13848d
kimHyeonJu <kimhyesom1202@gmail.com>
2025-10-24 11:08:50 +0900

    [Feature] sign-up
    - 회원가입 기능 추가

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from '@/components/signUpForm';
+
+export default function SignUpPage() {
+  return (
+    <section className="flex justify-center items-center w-full h-full">
+      <SignUpForm />
+    </section>
+  );
+}
