e2cd80d95c8139cde6f1bb4c3ff1cd9f8593e6d2
kimHyeonJu <kimhyesom1202@gmail.com>
2025-11-07 15:37:46 +0900

    [Feature] 학교 소개 관리 페이지
    - 학교 소개 관리 페이지, 레이아웃 제작

---
+++ src/app/admin/[schoolId]/introduction/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import Executive from '@/components/executive';
+import History from '@/components/history';
+import PageHeader from '@/components/pageHeader';
+import Symbol from '@/components/symbol';
+import BottomTab from '@/components/tab/bottom';
+import Vision from '@/components/vision';
+import { useSchoolStore } from '@/store/useSchoolStore';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { Suspense, useMemo } from 'react';
+
+export default function IntroductionPage() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { schoolId, activeTab } = useMemo(() => {
+    const segments = pathname.split('/').filter(Boolean);
+    return {
+      schoolId: segments[1],
+      activeTab: searchParams.get('tab'),
+    };
+  }, [pathname, searchParams.toString()]);
+
+  const { school } = useSchoolStore();
+
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <PageHeader title={`${school?.school_name} 소개`} />
+      <section
+        className="relative w-full h-full bg-white rounded-xl p-4 border border-border md:max-h-[728px] md:p-10"
+        aria-labelledby="department-info"
+      >
+        {activeTab === 'vision' && <Vision />}
+        {activeTab === 'history' && <History />}
+        {activeTab === 'symbol' && <Symbol />}
+        {activeTab === 'executive' && <Executive school={school} />}
+
+        {/* 하단 탭 */}
+        <footer className="fixed bottom-0 left-1/2 -translate-x-1/2 min-w-fit">
+          <BottomTab purpose="school_intro" />
+        </footer>
+      </section>
+    </Suspense>
+  );
+}
