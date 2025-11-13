c04973a262b0b2a6c0734c6569144c5388ad36b6
kimHyeonJu <kimhyesom1202@gmail.com>
2025-11-13 13:22:44 +0900

    [Feature] 학교 소개 관리 페이지
    - 연혁 페이지 리팩토링
    - 연혁 페이지 디테일 페이지 구현

--- src/app/admin/[schoolId]/introduction/history/page.tsx
+++ src/app/admin/[schoolId]/introduction/history/page.tsx
@@ -1,5 +1,9 @@
 import History from '@/components/history';
 
 export default function HistoryPage() {
-  return <History />;
+  return (
+    <div className="flex flex-col w-full">
+      <History />
+    </div>
+  );
 }
